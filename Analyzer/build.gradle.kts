3feabdffccaa2b80045620bdf95b22cd9bcf8e84
Alex Copindean <alexcopindean@yahoo.com>
2021-10-29 23:20:43 +0300

    Add analyzer base project

---
+++ Analyzer/build.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+    application
+}
+
+group = "me.akitektuo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "13"
+}
+
+application {
+    mainClass.set("MainKt")
+}
