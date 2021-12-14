9a6d156f609ea300de26619b67d186b7ddbe5457
Alex Copindean <alexcopindean@yahoo.com>
2021-12-14 04:36:48 +0200

    Add partial documentation

--- WebApi/Shared/Constants.cs
+++ WebApi/Shared/Constants.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebApi.Shared
+﻿namespace WebApi.Shared
 {
     public static class Constants
     {
