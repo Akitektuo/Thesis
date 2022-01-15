5abde847a3f6650634803a769e4eb866104b8966
Alex Copindean <alexcopindean@yahoo.com>
2022-01-15 15:24:24 +0200

    Add initial migration

--- WebApi/Controllers/TextLengthController.cs
+++ WebApi/Controllers/TextLengthController.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using WebApi.Aggregators;
 using WebApi.Shared;
 
