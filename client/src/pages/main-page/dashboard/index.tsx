a00a4f1308484462d05f22d69405401ab0e81094
Alex Copindean <alexcopindean@yahoo.com>
2022-02-27 21:52:41 +0200

    Arrange action buttons

--- client/src/pages/main-page/dashboard/index.tsx
+++ client/src/pages/main-page/dashboard/index.tsx
@@ -1,8 +1,10 @@
-import { Paper } from "@mui/material";
+import { Button, Paper } from "@mui/material";
 import classNames from "classnames";
-import { LetterAvatar, LevelIndicator, LoadingElement, Text } from "components";
+import { LetterAvatar, LevelIndicator, LoadingElement, LogoutButton, Text } from "components";
+import { useAuthentication } from "infrastructure";
 import { observer } from "mobx-react";
 import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router";
 import { DisplayBadgeType } from "shared/types/badge-types";
 import BadgeList from "./badge-list";
 import { UserDashboardContext } from "./user-dashboard-store";
@@ -45,6 +47,8 @@ const badges: DisplayBadgeType[] = [{
 }];
 
 const UserDashBoard = ({ className }: Props) => {
+    const navigate = useNavigate();
+    const { isUserAdmin } = useAuthentication();
     const { userDashboard, fetchUserDashboard } = useContext(UserDashboardContext);
 
     useEffect(() => {
@@ -61,6 +65,13 @@ const UserDashBoard = ({ className }: Props) => {
                 </div>
                 <div className={styles.badgesAndActions}>
                     <BadgeList badges={badges} />
+                    <div className={styles.actions}>
+                        <Button variant="outlined">See all</Button>
+                        {isUserAdmin && (
+                            <Button onClick={() => navigate("/admin")}>Admin page</Button>
+                        )}
+                        <LogoutButton />
+                    </div>
                 </div>
             </> : (
                 <LoadingElement className={styles.loading} show />
